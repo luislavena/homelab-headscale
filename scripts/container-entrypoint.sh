70f9b6fc3a82d6f0b780273d1a45939ca7ad71ea
Luis Lavena <luislavena@gmail.com>
2023-02-25 18:18:47 +0100

    Avoid break if DB already exists

--- scripts/container-entrypoint.sh
+++ scripts/container-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "---> Attempt to restore headscale database if missing..."
-litestream restore -if-replica-exists /data/headscale.sqlite3
+litestream restore -v -if-db-not-exists -if-replica-exists /data/headscale.sqlite3
 
 echo "---> Starting Headscale using Litestream..."
 exec litestream replicate -exec 'headscale serve'
